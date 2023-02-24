f18cea851558b2a69e098bd928388a71c70f566d
Steven Junio <Steven.junio91@gmail.com>
2023-02-24 01:32:11 +0000

    New test in init

--- elements/AAE-850ma/initialize.js
+++ elements/AAE-850ma/initialize.js
@@ -2,6 +2,7 @@ function initialize(instance, context) {
 
     console.log(`hello from ${instance.data.name}!`)
     console.log(`lets init again`)
+    console.log(`new one should make it in`)
 
 
 }
